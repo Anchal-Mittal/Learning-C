178b78bc1ea38f7f3f5b68d5dbca9df52178b8ff
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2018-02-07 21:22:11 +0530

    finding approaches

---
+++ average1.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+//********************************************APPROACH 1***************************************************
+
+long avr_approach1(int num1,int num2,int num3){
+
+  return  ((num1+num2+num3)/3) ;
+}
+//*******************************************APPROACH 2***************************************************  
+
+long avr_approach2(int num1,int num2,int num3){
+  return((num1/3)+(num2/3)+(num3/3)+((num1%3)+(num2%3)+(num3%3))/3);
+}
+
+//**********************************************MAIN FUNCTION *********************************************
+
+int main(){
+ int num1,num2,num3;
+ printf("%s \n","ENTER THE NUMBERS");
+ scanf("%d %d %d", &num1,&num2,&num3);
+ long result1=avr_approach1(num1,num2,num3);
+ long result2=avr_approach2(num1,num2,num3);
+ printf("%s,%d \n","THE RESULT OF FIRST APPROACH",result1);
+ printf("%s,%d \n","THE RESULT OF SECOND APPROACH",result2);
+  }
